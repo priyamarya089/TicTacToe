922e39ef9bd5e2367e1a62494a6a00cc6b2ad09b
priyamarya089 <51272216+priyamarya089@users.noreply.github.com>
2020-04-21 21:16:59 +0530

    winner message added

--- code.js
+++ code.js
@@ -2,12 +2,32 @@ const playerOne = 'X';
 const playerTwo = 'O';
 
 class TicTacToeGame {
-    updateBoard(){
+    checkGameWinner() {
+        for(let i=0;i<9;i=i+3){
+            if(this.board[i]!= "" && this.board[i+1]!= "" && this.board[i+2]!=""){
+                if(this.board[i] == this.board[i+1] && this.board[i+1] == this.board[i+2])return true;
+            }
+        }
+        for(let i=0;i<3;i++){
+            if(this.board[i]!= "" && this.board[i+3]!= "" && this.board[i+6]!=""){
+                if(this.board[i] == this.board[i+3] && this.board[i+3] == this.board[i+6])return true;
+            }
+        }
+        if(this.board[0]!= "" && this.board[4]!= "" && this.board[8]!=""){
+            if(this.board[0] == this.board[4] && this.board[4] == this.board[8])return true;
+        }
+        if(this.board[2]!= "" && this.board[4]!= "" && this.board[6]!=""){
+           if(this.board[2] == this.board[4] && this.board[4] == this.board[6])return true;
+        }
+        return false;
+    }
+
+    updateBoard() {
         let gameBoard = document.getElementById('gameBoard');
         let squareElements = gameBoard.childNodes;
-        console.log(squareElements);
-        squareElements.forEach((element , index )=>{
-            if(element.innerText != this.board[index]){
+        // console.log(squareElements);
+        squareElements.forEach((element, index) => {
+            if (element.innerText != this.board[index]) {
                 element.innerText = this.board[index];
             }
         });
@@ -18,12 +38,18 @@ class TicTacToeGame {
         let clickedId = event.target.id;
         if (this.board[clickedId] == "") {
             this.board[clickedId] = this.currentPlayer;
-            if (this.currentPlayer == playerOne) {
-                this.currentPlayer = playerTwo;
-            } else {
-                this.currentPlayer = playerOne;
-            }
             this.updateBoard();
+            if (!this.checkGameWinner()) {
+                if (this.currentPlayer == playerOne) {
+                    this.currentPlayer = playerTwo;
+                } else {
+                    this.currentPlayer = playerOne;
+                }
+            } else {
+                alert('Player ' + this.currentPlayer + ' is the winner');
+                //Resart the game
+                this.start();
+            }
         }
     }
 
@@ -53,4 +79,6 @@ class TicTacToeGame {
 }
 
 const game = new TicTacToeGame();
+game.start();
+new TicTacToeGame();
 game.start();
